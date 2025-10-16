7d64034bdb067764ad2a49f89d99f9970ed8cddd
Chenchang Wang <chenchangwang@ucsb.edu>
2025-10-16 12:01:28 -0700

    correct the output

--- src/main/java/jpa00/Hello.java
+++ src/main/java/jpa00/Hello.java
@@ -2,6 +2,7 @@ package jpa04;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("This is the wrong output!");
+        System.out.println("Hello, World!");
     }
 }
+
